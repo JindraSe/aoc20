3582b0f4d6439f582b4b47b075f218bf75836ec9
Jindřich Sedláček <514107@mail.muni.cz>
2023-06-28 07:05:36 +0200

    feat: finish day 7

--- src/day07/main.rs
+++ src/day07/main.rs
@@ -1,22 +1,34 @@
-use std::{path::Path, fs::read_to_string, collections::{HashMap, HashSet}, env::args};
+use std::{path::Path, fs::read_to_string, collections::{HashSet, HashMap}, env::args};
 
 struct BagRule {
     container_bag: String,
-    children: HashMap<String, u8>,
+    children: Vec<(String, usize)>,
+}
+
+impl BagRule {
+    fn to_map(bags: &Vec<BagRule>) -> HashMap<String, Vec<(String, usize)>> {
+        let mut result = HashMap::new();
+
+        for rule in bags {
+            result.insert(rule.container_bag.clone(), rule.children.clone());
+        }
+
+        return result;
+    }
 }
 
 fn skip_last(string_slice: &str, count: usize) -> &str {
     return &string_slice[..string_slice.len() - count];
 }
 
-fn parse_child_bag(child_bag: &str) -> (String, u8) {
+fn parse_child_bag(child_bag: &str) -> (String, usize) {
     let split_child_bag: Vec<&str> = child_bag.splitn(2, ' ').collect();
 
     if split_child_bag.len() != 2 {
         panic!("Wrong format of a child bag - no space!");
     }
 
-    let count = str::parse::<u8>(
+    let count = str::parse::<usize>(
         split_child_bag[0]
     ).expect("Wrong format of a child bag - not a number!");
 
@@ -36,7 +48,7 @@ fn parse_bag_rule(line: &str) -> BagRule {
 
     let container_bag = skip_last(split_line[0], 5).to_string();
     let children = if split_line[1] == "no other bags." {
-        HashMap::new()
+        Vec::new()
     } else {
         skip_last(split_line[1], 1).split(", ").map(parse_child_bag).collect()
     };
@@ -67,7 +79,7 @@ fn count_all_that_can_recursively_contain(bags: &Vec<BagRule>, name: &str) -> us
                 continue;
             }
 
-            if rule.children.keys().any(|name| candidates.contains(name)) {
+            if rule.children.iter().map(|(name, _)| name).any(|name| candidates.contains(name)) {
                 candidates.insert(rule.container_bag.to_string());
             }
         }
@@ -75,6 +87,29 @@ fn count_all_that_can_recursively_contain(bags: &Vec<BagRule>, name: &str) -> us
     }
 
     return candidates.len() - 1;
+}
+
+fn all_contained_by(bags: &Vec<BagRule>, name: &str) -> usize {
+    let bag_map = BagRule::to_map(bags);
+
+    let mut total_count = 0;
+
+    let mut current_layer: Vec<(String, usize)> = vec![(name.to_string(), 1)];
+    let mut next_layer: Vec<(String, usize)> = Vec::new();
+
+    while !current_layer.is_empty() {
+        for (name, count) in current_layer {
+            for (child_name, child_count) in bag_map[&name].iter() {
+                next_layer.push((child_name.clone(), child_count * count));
+            }
+        }
+
+        total_count += next_layer.iter().map(|(_, count)| count).sum::<usize>();
+        current_layer = next_layer;
+        next_layer = Vec::new();
+    }
+
+    return total_count;
 }
 
 fn main() {
@@ -88,7 +123,9 @@ fn main() {
 
     let rules = load_rules(path_to_input);
 
-    let count = count_all_that_can_recursively_contain(&rules, "shiny gold");
+    let containing_count = count_all_that_can_recursively_contain(&rules, "shiny gold");
+    println!("The count of possible outer bag colors is: {}", containing_count);
 
-    println!("The count of possible outer bag colors is: {}", count);
+    let contained_count = all_contained_by(&rules, "shiny gold");
+    println!("The shiny gold bag will contain {} bags.", contained_count);
 }
